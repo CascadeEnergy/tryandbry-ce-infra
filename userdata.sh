a4eff256ae06e06717cae0bc7608ccaacffdb88f
Bryan Kao <bryan.kao@cascadeenergy.com>
2024-12-18 15:06:51 -0600

    userdata working!

--- userdata.sh
+++ userdata.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-## NOTE: WIP as of 12/18/2024
-
+##### as root
 ## working directory
-sudo mkdir -p /var/cascade
-sudo chown ec2-user /var/cascade
+mkdir -p /var/cascade
+chown ec2-user /var/cascade
 
 ## git
-sudo dnf install -y git
+dnf install -y git
 
 ## gh cli
-type -p yum-config-manager >/dev/null || sudo yum install yum-utils
-sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo yum install gh
+type -p yum-config-manager >/dev/null || yum install yum-utils
+yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+yum install -y gh
 
+##### change to ec2-user
+sudo -i -u ec2-user bash << EOF
 ## nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 ## enable use of nvm in current session
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /home/ec2-user/.bashrc
 
 ## install node
 nvm install 18
+
+## configure npm
 npm config set legacy-peer-deps=true
 
 ## configure git
@@ -31,3 +32,4 @@ echo .DS_Store > ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 git config --global user.name "Bryan Kao"
 git config --global user.email "bryan.kao@cascadeenergy.com"
+EOF
